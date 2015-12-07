3bc53bbe167f48ca950bc08396f076937ca25251
cesartejeda <cesartejeda62@gmail.com>
2015-12-07 15:07:18 -0700

    Yii

---
+++ MiAplicacion/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;port=5432;dbname=basededatosyii',
+    'username' => 'cesar',
+    'password' => 'cesartejeda',
+    'charset' => 'utf8',
+];
